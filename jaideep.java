0b5d7876fa0e43a111a06c59dc72b9b39f8e7bed
Jaideep-ai <jaideepsingh141410@gmail.com>
2025-02-01 11:09:24 +0530

    version 3

--- jaideep.java
+++ jaideep.java
@@ -1,7 +1,7 @@
 public class jaideep {
     public static void main (String [] args) {
         System.out.println("Jaideep Singh");
-        System.out.println("version -- 2 ");
+        System.out.println("version -- 3 ");
     }
     
 }
